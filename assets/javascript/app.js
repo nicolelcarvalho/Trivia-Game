94da1a593032f9b256900a673e562d849c092ec6
Nicole Carvalho <NicoleCarvalho@Nicole-Carvalhos-MacBook-Pro.local>
2017-09-06 09:59:23 -0400

    pseudocoded the steps in the .js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// Write a question to the screen and provide answer choices. 
+// Write each question in a function. 
+// Display the time that is left to answer the question. 
+
+// Set each function to a timer. Delay each question so that the user has time to answer. 
+
+// Set conditionals. 
+// If the user clicks an answer correctly, show a gif related to that answer. 
+// If the user clicks incorrectly, show a gif that they got it wrong. 
+
+// At the end of the game, show a page that displays the number of answers the user answered correctly and incorrectly. 
+// Provide a button to restart the game without reloading the screen. 
